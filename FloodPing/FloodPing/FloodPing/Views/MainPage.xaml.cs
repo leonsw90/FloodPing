49ecaa43cbac723d496b28235164729fecaf61ee
Vimal Narayan <vimal.narayan@gmail.com>
2016-05-12 07:41:15 +1000

    Start adding the application scaffolding

---
+++ FloodPing/FloodPing/FloodPing/Views/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace FloodPing.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void LaunchTraveller(object sender, EventArgs args)
+        {
+            // Launch Traveller App
+        }
+
+        public void LaunchEmergency(object sender, EventArgs args)
+        {
+           // Launch Emergency App
+        }
+    }
+}
