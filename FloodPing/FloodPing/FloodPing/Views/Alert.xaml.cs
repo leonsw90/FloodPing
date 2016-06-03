102fd8b2de209721a83d1d0a5364eb740d043838
torbecire <bemmusobo@gmail.com>
2016-06-03 16:29:47 +1000

    Add files via upload

---
+++ FloodPing/FloodPing/FloodPing/Views/Alert.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FloodPing.Views
+{
+	public partial class Alert : ContentPage
+	{
+		public Alert ()
+		{
+			this.BackgroundImage = "AlertPhone.png";
+			BindingContext = this;
+
+			InitializeComponent ();
+			this.Title = "WARNING";
+		}
+
+		public void LaunchFloodAlert2(object sender, EventArgs args)
+		{
+			// Launch Emergency App
+			Navigation.PushAsync(new Views.Alert2());
+		}
+	}
+}
+
---
+++ FloodPing/FloodPing/FloodPing/Views/Alert2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FloodPing.Views
+{
+	public partial class Alert2 : ContentPage
+	{
+		public Alert2()
+		{
+			this.BackgroundImage = "floodplace.png";
+			BindingContext = this;
+
+			InitializeComponent ();
+			this.Title = "WARNING";
+		}
+
+		public void LaunchFloodAlert3(object sender, EventArgs args)
+		{
+			// Launch Emergency App
+			Navigation.PushAsync(new Views.Alert3());
+		}
+	}
+}
+
